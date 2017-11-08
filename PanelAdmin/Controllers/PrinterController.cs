fffe645c82d789849c79190c5dc9e15bdf0e1eea
Wilson <wilsonvargas_6@outlook.com>
2017-11-08 00:52:04 -0500

    Added web project for panel admin

---
+++ PanelAdmin/Controllers/PrinterController.cs
@@ -0,0 +1,23 @@
+﻿using EPrint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PanelAdmin.Controllers
+{
+    public class PrinterController : Controller
+    {
+        // GET: Printer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
